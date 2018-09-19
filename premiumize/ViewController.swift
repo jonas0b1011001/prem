c0c23ead6e91eeb29819a8596f00647bc09bf6e1
Jonas Geissler <jonas@jonass-macbook-pro.local>
2018-09-19 22:20:50 +0200

    login added

--- premiumize/ViewController.swift
+++ premiumize/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     fileprivate var resp: String = "loading..."
+    fileprivate var id: String = "ID"
+    fileprivate var pin: String = "PIN"
     @IBOutlet weak var lblStatus: UILabel!
     
+    @IBOutlet weak var txtID: UITextField!
+    @IBOutlet weak var txtPin: UITextField!
+    @IBAction func btnSave(_ sender: Any) {
+        UserDefaults.standard.set(txtID.text, forKey: "id")
+        UserDefaults.standard.set(txtPin.text, forKey: "pin")
+    }
     @IBAction func btnAction(_ sender: UIButton) {
         getAccountInfo()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.txtPin.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        txtPin.text = UserDefaults.standard.string(forKey: "pin") ?? ""
+        txtID.text = UserDefaults.standard.string(forKey: "id") ?? ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,8 +39,10 @@ class ViewController: UIViewController {
     }
     
     func getAccountInfo() {
+        id = txtID.text ?? ""
+        pin = txtPin.text ?? ""
         let configuration = URLSessionConfiguration.ephemeral
-        let url = URL(string: "https://www.premiumize.me/api/folder/list?customer_id=xxxxxxx&pin=zjgu884n4yfxsezg")!
+        let url = URL(string: "https://www.premiumize.me/api/folder/list?customer_id=\(id)&pin=\(pin)")!
         let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: url, completionHandler: { [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
             guard let data = data else {
@@ -59,6 +72,9 @@ class ViewController: UIViewController {
         })
         task.resume()
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
